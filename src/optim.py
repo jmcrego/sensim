41057400c7a091e2386ac667e790e2f509f7c91a
jmcrego <jmcrego@gmail.com>
2020-01-30 10:08:08 +0100

    masks

--- src/optim.py
+++ src/optim.py
@@ -143,19 +143,22 @@ class ComputeLossSim:
         print('slen',slen.size())
         print('tlen',tlen.size())
         print('y',y.size())
+        mask_s = sequence_mask(slen,mask_n_initials=2).unsqueeze(-1)
+        mask_t = sequence_mask(tlen,mask_n_initials=2).unsqueeze(-1)
+        print('mask_s',mask_s.size())
+        print('mask_t',mask_t.size())
+
         if self.opt is not None:
             self.opt.optimizer.zero_grad()
 
         if self.pooling == 'max':
-            mask_s = sequence_mask(slen,mask_n_initials=2).unsqueeze(-1)
-            mask_t = sequence_mask(tlen,mask_n_initials=2).unsqueeze(-1)
             s, _ = torch.max(hs * mask_s + (1-mask_s) * torch.float('-Inf'), dim=1)
             t, _ = torch.max(ht * mask_t + (1-mask_t) * torch.float('-Inf'), dim=1)
             loss = self.criterion(s, t, y)
 
         elif self.pooling == 'mean':
-            s = torch.mean(hs * sequence_mask(slen,mask_n_initials=2).unsqueeze(-1), dim=1)
-            t = torch.mean(ht * sequence_mask(tlen,mask_n_initials=2).unsqueeze(-1), dim=1)
+            s = torch.sum(hs * mask_s, dim=1) / torch.sum(mask_s)
+            t = torch.sum(ht * mask_t, dim=1) / torch.sum(mask_t)
             loss = self.criterion(s, t, y)
 
         elif self.pooling == 'cls':
@@ -167,10 +170,16 @@ class ComputeLossSim:
             S = torch.bmm(hs, torch.transpose(ht, 2, 1)) #[bs, sl, es] x [bs, es, tl] = [bs, sl, tl]
             #st_mask is [bs, sl, tl] containing True for words to consider and False for words to mask (<pad>, <bos>, <eos>)
             aggr = self.aggr(S,st_mask(slen,tlen,mask_n_initials=2)) #equation (2) #for each tgt word, consider the aggregated matching scores over the source sentence 
+            sign = torch.ones(aggr.size()) * y.unsqueeze(-1)
+            print('sign',sign.size())
             error = torch.log(1 + torch.exp(aggr * sign)) #equation (3) error of each tgt word
-            #sum_error = tf.map_fn(lambda (x, l): tf.reduce_sum(x[2:l-1]), (error_tgt, tl))
-            loss = torch.sum(sum_error * mask_t) / torch.sum(mask_t)
-            #loss = self.criterion(s, t, y)
+            print('error',error.size())
+            sum_error = torch.sum(error * mask_t.squeeze(), dim=1)
+            print('sum_error',sum_error.size())
+#            loss = torch.sum(sum_error * mask_s) / torch.sum(mask_s) ????? mask_s ?
+            loss = torch.mean(sum_error)
+            print('loss',loss.size())
+            sys.exit()
 
         else:
             logging.error('bad pooling method {}'.format(self.pooling))
@@ -185,13 +194,10 @@ class ComputeLossSim:
     def aggr(self,S_st,mask_st):
         print('S_st',S_st.size()) #[bs, ls, lt]
         print('mask_st',mask_st.size()) #[bs, ls, lt] contains zero those cells to be padded
-        #print('l_s',l_s.size()) #[bs] #lengths of source sentences 
-        #The aggregation operation summarizes the alignment scores for each target word
+        ### The aggregation operation summarizes the alignment scores for each target word
         exp_rS = torch.exp(S_st * self.R)
         print('exp_rS',exp_rS.size()) #[bs,ls,lt]
-
         #sum_exp_rS = tf.map_fn(lambda (x, l): tf.reduce_sum(x[2:l-1, :], 0), (exp_rS, l_s)) #[B,Ss] (do not sum over <bos> and <eos> [2:l-1])
-        mask_zeros = torch.zeros(S_st.size(), requires_grad=False)
         sum_exp_rS = torch.sum(exp_rS * mask_st,dim=1) #sum over source words (dim=1)
         print('sum_exp_rS',sum_exp_rS.size()) #[bs,lt]
         log_sum_exp_rS = torch.log(sum_exp_rS) 
@@ -206,8 +212,8 @@ def sequence_mask(lengths, mask_n_initials=0):
     msk = torch.ones([len(lengths), maxlen]).cumsum(dim=1, dtype=torch.int32).t()
     #for a 3x3 matrix: msk = [[1,2,3],[1,2,3],[1,2,3]]
     if mask_n_initials > 0:
-        return ((msk <= lengths) & (msk > mask_n_initials)) #.t().type(torch.bool)
-    return (msk <= lengths) #.t().type(torch.bool)
+        return ((msk <= lengths) & (msk > mask_n_initials)).t().type(torch.bool)
+    return (msk <= lengths).t().type(torch.bool)
 
 
 def st_mask(slen,tlen,mask_n_initials=0):
