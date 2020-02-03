61459e597ef6db1f497e09989ca51b3dcf4db179
jmcrego <jmcrego@gmail.com>
2020-02-03 15:52:14 +0100

    crossentropy

--- src/trainer.py
+++ src/trainer.py
@@ -210,7 +210,7 @@ class Trainer():
                 h1 = self.model.forward(x1,x1_mask)
                 h2 = self.model.forward(x2,x2_mask)
                 batch_loss = self.computeloss(h1, h2, l1, l2, y, mask_s, mask_t)
-                del x1, x2, l1, l2 x1_mask, x2_mask, y, mask_s, mask_t
+                del x1, x2, l1, l2, x1_mask, x2_mask, y, mask_s, mask_t
             torch.cuda.empty_cache()
             ds.add_batch(batch_loss,n_predictions)
         ds.report(self.n_steps_so_far,step,'Valid')
