91a53c6418bc062b26802f1b847f464792c1b9c8
jmcrego <jmcrego@gmail.com>
2020-02-04 12:09:32 +0100

    problem with clone free

--- src/optim.py
+++ src/optim.py
@@ -171,7 +171,8 @@ class ComputeLossSIM:
     def aggr(self,S_st,mask_s): #foreach tgt word finds the aggregation over all src words
         print('S_st',S_st[1])
         #print('mask_s',mask_s[0])
-        S_st_limited = torch.min(S_st, (torch.ones(S_st.size(), device=S_st.device)*9.9))
+        maximum = torch.ones(S_st.size(), device=S_st.device) * 9.9
+        S_st_limited = torch.min(S_st, maximum)
         print('S_st_limited',S_st_limited[1])
         exp_rS = torch.exp(S_st_limited * self.R)  ### attention!!! exp(large number) = nan
         print('exp_rS',exp_rS[1])
@@ -179,8 +180,10 @@ class ComputeLossSIM:
         print('sum_exp_rS',sum_exp_rS[1])
         log_sum_exp_rS_div_R = torch.log(sum_exp_rS) / self.R
         print('log_sum_exp_rS_div_R',log_sum_exp_rS_div_R[1])
-        log_sum_exp_rS_div_R_limited, _ = torch.max(log_sum_exp_rS_div_R, (torch.ones(log_sum_exp_rS_div_R.size(), device=S_st.device)*-99.9))
+        minimum = torch.ones(log_sum_exp_rS_div_R.size(), dtype=torch.float64, device=S_st.device) * -99.9
+        log_sum_exp_rS_div_R_limited, _ = torch.max(log_sum_exp_rS_div_R, minimum)
         print('log_sum_exp_rS_div_R_limited',log_sum_exp_rS_div_R_limited[1])
+
         return log_sum_exp_rS_div_R_limited
 
 
