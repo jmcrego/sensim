9734d95c987e213fd48bcb966636e013eea6a45e
jmcrego <jmcrego@gmail.com>
2020-02-04 11:12:49 +0100

    problem with clone free

--- src/optim.py
+++ src/optim.py
@@ -155,8 +155,10 @@ class ComputeLossSIM:
 
         elif self.pooling == 'align':
             S_st = torch.bmm(hs, torch.transpose(ht, 2, 1)) #[bs, sl, es] x [bs, es, tl] = [bs, sl, tl]            
+            print('S_st',S_st[0])
+            print('mask_s',mask_s[0])
             aggr_t = self.aggr(S_st,mask_s) #equation (2) #for each tgt word, consider the aggregated matching scores over the source sentence words
-            print(aggr_t[0])
+            print('aggr_t',aggr_t[0])
             sys.exit()
             loss = self.criterion(aggr_t,y,mask_t.squeeze())
 
@@ -167,9 +169,13 @@ class ComputeLossSIM:
         return loss #not normalized
 
     def aggr(self,S_st,mask_s): #foreach tgt word finds the aggregation over all src words
+        print('S_st',S_st[0])
         exp_rS = torch.exp(S_st * self.R)
+        print('exp_rS',exp_rS[0])
         sum_exp_rS = torch.sum(exp_rS * mask_s,dim=1) #sum over all source words (source words nor used are masked)
+        print('sum_exp_rS',sum_exp_rS[0])
         log_sum_exp_rS_div_R = torch.log(sum_exp_rS) / self.R
+        print('log_sum_exp_rS_div_R',log_sum_exp_rS_div_R[0])
         return log_sum_exp_rS_div_R
 
 
