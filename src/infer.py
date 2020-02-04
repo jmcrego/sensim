4bc7b7839a9c7b6ad98c3b4cfbc4c468610a94ec
jmcrego <jmcrego@gmail.com>
2020-02-04 20:49:31 +0100

    inference

--- src/infer.py
+++ src/infer.py
@@ -119,7 +119,7 @@ class Infer():
                     print(' '.join([str(tok) for tok in sentence]))
                 elif len(src_tgt)>1:
                     sim = cos(s,t)
-                    print(sim[0])
+                    print(sim[0].data)
 
         logging.info('End validation')
 
