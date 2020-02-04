9e63f4333314fafdcba1e88bff2aa50a1d5ca1af
jmcrego <jmcrego@gmail.com>
2020-02-04 17:51:04 +0100

    inference

--- src/infer.py
+++ src/infer.py
@@ -70,6 +70,9 @@ class Infer():
                 x = torch.from_numpy(batch_src) #[batch_size, max_len] the original words with padding
                 x_mask = torch.as_tensor((x != self.vocab.idx_pad)).unsqueeze(-2) #[batch_size, 1, max_len]
                 mask_s = torch.from_numpy(sequence_mask(batch_src_len,mask_n_initials=2))
+                print(x)
+                print(x_mask)
+                print(mask_s)
 
                 if self.cuda:
                     x.cuda()
