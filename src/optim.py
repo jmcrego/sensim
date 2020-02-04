954c4aa4ee10c4f0a3dc855488137defd3c8e1b9
jmcrego <jmcrego@gmail.com>
2020-02-04 12:37:18 +0100

    problem with clone free

--- src/optim.py
+++ src/optim.py
@@ -159,7 +159,7 @@ class ComputeLossSIM:
             S_st = torch.bmm(hs, torch.transpose(ht, 2, 1)) #[bs, sl, es] x [bs, es, tl] = [bs, sl, tl]            
             aggr_t = self.aggr(S_st,mask_s) #equation (2) #for each tgt word, consider the aggregated matching scores over the source sentence words
             loss = self.criterion(aggr_t,y,mask_t.squeeze())
-            print('loss',loss)
+#            print('loss',loss)
 #            sys.exit()
 
         else:
@@ -169,18 +169,18 @@ class ComputeLossSIM:
         return loss #not normalized
 
     def aggr(self,S_st,mask_s): #foreach tgt word finds the aggregation over all src words
-        print('mask_s',mask_s[1])
-        print('S_st',S_st[1])
-        S_st[S_st > 9.9] = 9.9 ### attention!!! exp(large number) = nan
-        print('S_st limited',S_st[1])
+        #print('mask_s',mask_s[1])
+        #print('S_st',S_st[1])
+        #S_st[S_st > 9.9] = 9.9 ### attention!!! exp(large number) = nan
+        #print('S_st limited',S_st[1])
         exp_rS = torch.exp(S_st * self.R)
-        print('exp_rS',exp_rS[1])
+        #print('exp_rS',exp_rS[1])
         sum_exp_rS = torch.sum(exp_rS * mask_s,dim=1) #sum over all source words (source words nor used are masked)
-        print('sum_exp_rS',sum_exp_rS[1])
+        #print('sum_exp_rS',sum_exp_rS[1])
         log_sum_exp_rS_div_R = torch.log(sum_exp_rS) / self.R
-        print('log_sum_exp_rS_div_R',log_sum_exp_rS_div_R[1])
-        log_sum_exp_rS_div_R[log_sum_exp_rS_div_R < -9.9] = -9.9 ### attention!!! log(zero) = nan
-        print('log_sum_exp_rS_div_R limited',log_sum_exp_rS_div_R[1])
+        #print('log_sum_exp_rS_div_R',log_sum_exp_rS_div_R[1])
+        #log_sum_exp_rS_div_R[log_sum_exp_rS_div_R < -9.9] = -9.9 ### attention!!! log(zero) = nan
+        #print('log_sum_exp_rS_div_R limited',log_sum_exp_rS_div_R[1])
         return log_sum_exp_rS_div_R
 
 
