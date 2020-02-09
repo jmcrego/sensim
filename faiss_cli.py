2af7257e19ec40bd716d5badfbc23e44f875dcc7
jmcrego <jmcrego@gmail.com>
2020-02-09 14:18:52 -0500

    add distance

--- faiss_cli.py
+++ faiss_cli.py
@@ -33,12 +33,10 @@ class IndexFaiss:
     #build an index with METRIC_INNER_PRODUCT
     #normalize the vectors prior to adding them to the index (with faiss.normalize_L2 in Python)
     #normalize the vectors prior to searching them
-        self.index = faiss.IndexFlatIP(d)       # build the index L2
-        self.index.add(faiss.normalize_L2(np.array(self.db).astype('float32')))
 
-#       self.index = faiss.IndexFlatIP(d)       # build the index L2
-#       self.db = np.array(self.db).astype('float32')
-#       self.index.add(self.db) # add vectors to the index
+        self.index = faiss.IndexFlatIP(d)       # build the index L2
+        self.db = np.array(self.db).astype('float32')
+        self.index.add(faiss.normalize_L2(self.db)) # add vectors to the index
         logging.info("read {} vectors".format(self.index.ntotal))
 
 
