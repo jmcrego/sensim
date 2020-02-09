2e58ba8e6a3d59e96677db05bc3d69e9d11dedb7
jmcrego <jmcrego@gmail.com>
2020-02-09 18:35:36 -0500

    add distance

--- faiss_cli.py
+++ faiss_cli.py
@@ -32,15 +32,10 @@ class IndexFaiss:
             for l in f:
                 self.db_str.append(l.rstrip())
 
-        self.index = faiss.IndexFlatIP(d) #inner product (needs vectors normalization)
+        self.index = faiss.IndexFlatIP(d) #inner product (needs L2 normalization over db and query vectors)
         self.db = np.array(self.db).astype('float32')
-        print(self.db_str[99])
-        print(' '.join('{:.4f}'.format(x) for x in self.db[99]))
         faiss.normalize_L2(self.db)
-        print(' '.join('{:.4f}'.format(x) for x in self.db[99]))
-        sys.exit()
-        print(self.db[99])
-        self.index.add(self.db) # add vectors to the index
+        self.index.add(self.db) # add all normalized vectors to the index
         logging.info("read {} vectors".format(self.index.ntotal))
 
 
