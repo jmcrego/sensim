d8b2974e27f528226db4100aab9fb62250ec18a8
jmcrego <jmcrego@gmail.com>
2020-02-09 14:53:41 -0500

    add distance

--- faiss_cli.py
+++ faiss_cli.py
@@ -46,7 +46,7 @@ class IndexFaiss:
 
     def Query(self,file,d,k,file_str):
         if file.endswith('.gz'): 
-            f = gzip.open(fsrc, 'rb')
+            f = gzip.open(fsrc, 'r')
         else:
             f = io.open(file, 'r', encoding='utf-8', newline='\n', errors='ignore')
 
@@ -61,7 +61,7 @@ class IndexFaiss:
         query_str = []
         if file_str is not None:
             if file_str.endswith('.gz'): 
-                f = gzip.open(file_str, 'rb')
+                f = gzip.open(file_str, 'r')
             else:
                 f = io.open(file_str, 'r', encoding='utf-8', newline='\n', errors='ignore')
             for l in f:
