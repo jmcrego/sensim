5d3757e2a1a676830c7e46561b5b46839ab9e590
jmcrego <jmcrego@gmail.com>
2020-02-11 12:11:48 -0500

    light

--- src/infer.py
+++ src/infer.py
@@ -141,7 +141,7 @@ class Infer():
                         align = []
                         align.append(src + [''])
                         for t in range(len(tgt)):
-                            line = S_st[0,2:-2,t]
+                            line = S_st[0,2:-1,t]
                             print('line',line)
                             sys.exit()
                             line.append(src[s])
