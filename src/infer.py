8bbe4a9681b30de4faf1e20dc47a4658cd41a5cc
jmcrego <jmcrego@gmail.com>
2020-02-11 22:38:23 -0500

    infer

--- src/infer.py
+++ src/infer.py
@@ -125,9 +125,9 @@ class Infer():
                     S_st = torch.bmm(h1, torch.transpose(h2, 2, 1)) * self.align_scale #[bs, sl, es] x [bs, es, tl] = [bs, sl, tl]            
                     ### scale S_st to <=10
                     mask_s = mask_s.type(torch.float64)
-                    S_st_masked_s = S_st * mask_s
-                    S_st_masked_st = S_st_masked_s * mask_t.transpose(2,1)
-                    max_div_10 = torch.max(S_st_masked_st.pow(2)).pow(0.5) / 10.0
+                    S_st_masked_s = S_st * mask_s #[bs, ls, lt] * [bs, ls, 1]
+                    S_st_masked_st = S_st_masked_s * mask_t.transpose(2,1) #[bs, ls, lt] * [bs, 1, lt]
+                    max_div_10 = torch.max(S_st_masked_st.pow(2)).pow(0.5) / 1.0 ### largest number will be 10.0
                     S_st = S_st / max_div_10
                 else:
                     logging.error('bad pooling method: {}'.format(self.pooling))
